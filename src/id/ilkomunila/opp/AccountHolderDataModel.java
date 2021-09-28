e4a21a6d58a1ef825d76fc6f18a2e6c5113da892
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-09-28 16:33:28 +0700

    add getIndividualHolder function

--- src/id/ilkomunila/opp/AccountHolderDataModel.java
+++ src/id/ilkomunila/opp/AccountHolderDataModel.java
@@ -1,10 +1,16 @@
 package id.ilkomunila.opp;
 
 import id.ilkomunila.opp.db.DBHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AccountHolderDataModel {
     private final Connection conn;
@@ -66,4 +72,34 @@ public class AccountHolderDataModel {
         stmtAccount.execute();
     }
 
+    public ObservableList<IndividualHolder> getIndividualHolder {
+        ObservableList<IndividualHolder> data = FXCollections.observableArrayList();
+        String sql = "SELECT holder_id, name, address, gender, birthdate "
+                + "FROM account_holder NATURAL JOIN individual_holder "
+                + "ORDER BY name";
+
+        try () {
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            while (rs.next()) {
+                String sqlAccount = "SELECT acc_number, balance FROM account WHERE holder_id="
+                        + rs.getInt(1);
+                ResultSet rsAccount = conn.createStatement().executeQuery(sqlAccount);
+                ArrayList<Account> dataAccount = new ArrayList<>();
+                while (rsAccount.next()) {
+                    dataAccount.add(new Account(rsAccount.getInt(1), rsAccount.getDouble(2)));
+                }
+                data.add(new IndividualHolder(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        dataAccount,
+                        rs.getString(4),
+                        rs.getString(5)
+                ));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountHolderDataModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return data;
+    }
 }
